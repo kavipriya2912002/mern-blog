a2988f10de31d4d6c5504241d973aa1b15a63b4e
kavipriya2912002 <kavipriya2912002@gmail.com>
2024-02-29 10:13:31 +0530

    add showmore functionality

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -86,51 +86,51 @@ export default function DashPosts() {
                 <span>Edit</span>
               </Table.HeadCell>
             </Table.Head>
-            {userPosts.map((post) => (
-              <Table.Body className='divide-y'>
-                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
-                  <Table.Cell>
-                    {new Date(post.updatedAt).toLocaleDateString()}
-                  </Table.Cell>
-                  <Table.Cell>
-                    <Link to={`/post/${post.slug}`}>
-                      <img
-                        src={post.image}
-                        alt={post.title}
-                        className='w-20 h-10 object-cover bg-gray-500'
-                      />
-                    </Link>
-                  </Table.Cell>
-                  <Table.Cell>
-                    <Link
-                      className='font-medium text-gray-900 dark:text-white'
-                      to={`/post/${post.slug}`}
-                    >
-                      {post.title}
-                    </Link>
-                  </Table.Cell>
-                  <Table.Cell>{post.category}</Table.Cell>
-                  <Table.Cell>
-                    <span
-                      onClick={() => {
-                        setShowModal(true);
-                        setPostIdToDelete(post._id);
-                      }}
-                      className='font-medium text-red-500 hover:underline cursor-pointer'
-                    >
-                      Delete
-                    </span>
-                  </Table.Cell>
-                  <Table.Cell>
-                    <Link
-                      className='text-teal-500 hover:underline'
-                      to={`/update-post/${post._id}`}
-                    >
-                      <span>Edit</span>
-                    </Link>
-                  </Table.Cell>
-                </Table.Row>
-              </Table.Body>
+            {userPosts.map((post,i) => (
+               <Table.Body className='divide-y' key={i}>
+               <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                 <Table.Cell>
+                   {new Date(post.updatedAt).toLocaleDateString()}
+                 </Table.Cell>
+                 <Table.Cell>
+                   <Link to={`/post/${post.slug}`}>
+                     <img
+                       src={post.image}
+                       alt={post.title}
+                       className='w-20 h-10 object-cover bg-gray-500'
+                     />
+                   </Link>
+                 </Table.Cell>
+                 <Table.Cell>
+                   <Link
+                     className='font-medium text-gray-900 dark:text-white'
+                     to={`/post/${post.slug}`}
+                   >
+                     {post.title}
+                   </Link>
+                 </Table.Cell>
+                 <Table.Cell>{post.category}</Table.Cell>
+                 <Table.Cell>
+                   <span
+                     onClick={() => {
+                       setShowModal(true);
+                       setPostIdToDelete(post._id);
+                     }}
+                     className='font-medium text-red-500 hover:underline cursor-pointer'
+                   >
+                     Delete
+                   </span>
+                 </Table.Cell>
+                 <Table.Cell>
+                   <Link
+                     className='text-teal-500 hover:underline'
+                     to={`/update-post/${post._id}`}
+                   >
+                     <span>Edit</span>
+                   </Link>
+                 </Table.Cell>
+               </Table.Row>
+             </Table.Body>
             ))}
           </Table>
           {showMore && (
